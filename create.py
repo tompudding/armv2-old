b75b614b9af75be6fe62ffc053bc7e3f9db36274
Tom Gooding <tom.pudding@gmail.com>
2013-05-19 01:27:27 +0100

    Added a step to remove 'bx lr' from the gcc stdlib

--- create.py
+++ create.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import struct
 
 def load(filename):
     with open(filename,'rb') as f:
         return f.read()
 
 boot,data = (load(filename) for filename in sys.argv[1:3])
+
+#Get rid of any bx lrs from the gcc stdlib
+
+data = data.replace(struct.pack('<I',0xe12fff1e),struct.pack('<I',0xe1a0f00e))
 
 assert len(boot) < 0x8000
 boot = boot + '\x00'*(0x8000 - len(boot))
