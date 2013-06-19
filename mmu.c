7e2945af2ef38d21a769a71d2e7c920775d994e3
Tom Gooding <tom.pudding@gmail.com>
2013-06-19 23:52:53 +0100

    Added new coprocessor routine handlers

---
+++ mmu.c
@@ -0,0 +1,7 @@
+#include "armv2.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+armv2status_t MmuDataOperation         (uint32_t crm, uint32_t aux, uint32_t crd, uint32_t crn, uint32_t opcode) {
+    return ARMV2STATUS_OK;
+}
