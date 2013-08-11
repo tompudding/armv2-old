d8f936139e7cdec2c11cf11713ec2cab09ffe25f
Tom Gooding <tom.pudding@gmail.com>
2013-08-11 06:47:18 -0700

    Added hardware.py

---
+++ hardware.py
@@ -0,0 +1,15 @@
+import armv2
+
+class Keyboard(armv2.Device):
+    id = 0x41414141
+    def __init__(self):
+        super(Keyboard,self).__init__()
+        armv2.DebugLog('keyboard keyboard keyboard!\n')
+
+    def readCallback(self,addr,value):
+        armv2.DebugLog('keyboard reader %x %x\n' % (addr,value))
+
+    def writeCallback(self,addr,value):
+        armv2.DebugLog('keyboard writer %x %x\n' % (addr,value))
+
+    
